f98f06cae5eb534c1549182151e7fbcbc486b842
aditya2695 <adityaarajan@gmail.com>
2017-02-06 20:23:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,23 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
+
+button.onclick=function(){
+
+    var request = new XMLHttpRequest();
+
+//CAPTURE the response and store it in a variable
+request.onreadystatechange = function()
+{
+    if (request.readyState === XMLHttpRequest.DONE){
+        //take some action
+        if(request.status ===200){
+            var counter=request.responseTest;
+            var span=document.getElementById('count');
+            span.innerHTML = counter.toString();
+        
+        }
+    }
+}
+
+//Make the request
+request.open('GET', 'http://aditya2695.imad.hasura-app.io/counter',true);
+}
